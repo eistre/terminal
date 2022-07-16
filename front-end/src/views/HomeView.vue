99f4e776ed5b86d43b11dceddd78d0dd70174b95
Joonas Halapuu <joonas.halapuu16@gmail.com>
2022-07-16 17:05:19 +0300

    Closes #13. Created component for landingpage login buttons

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,76 +1,14 @@
 <script>
 import Banner from '@/components/Banner.vue'
+import LoginButtons from '../components/LoginButtons.vue';
 
 export default {
-  components: {
-    Banner
-  },
-  data() {
-    return {
-      HOST: import.meta.env.VITE_HOST,
-    };
-  },
-  methods: {
-    //HTML request script taken from https://stackoverflow.com/questions/45697176/send-simple-http-request-with-html-submit-button
-    //      Credit goes to thepi
-    // Authentication modification by Joonas.
-    //TODO: can the POST action be done inside html using <form action="/ubuntuInstance/Unknown" method="post">
-    sendRequest: function (name, matriculationNr) {
-      const authButton = document.getElementById("authButton");
-      const anonButton = document.getElementById("anonButton");
-      authButton.disabled = true;
-      anonButton.disabled = true;
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true; //so the cookies can be used.
-      if (matriculationNr)
-        xhr.open("POST", `http://${this.HOST}:8080/ubuntuInstance/${matriculationNr}`, true);
-      else
-        xhr.open("POST", `http://${this.HOST}:8080/ubuntuInstance/anonymous`, true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(name ? JSON.stringify({ name: name }) : null);
-      var trueRouter = this.$router;
-      xhr.onload = function () {
-        authButton.disabled = false;
-        anonButton.disabled = false;
-        if (this.status === 508) {
-          alert(`${this.responseText}`);
-          return;
-        }
-        var data = JSON.parse(this.response);
-        if (matriculationNr)
-          trueRouter.replace(`/terminal?port=${data.port}&name=${name}&mat=${matriculationNr}`);
-        else
-          trueRouter.replace(`/terminal?port=${data.port}&name=külaline`);
-      };
-      //Log it!
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true; //so the cookies can be used.
-      if (matriculationNr) {
-        xhr.open("PUT", `http://${this.HOST}:8080/logger`, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({ matriculation: matriculationNr, taskNr: 0 }));
-      }
-    },
-    validateLoginCredentials: function () {
-      var name = document.getElementById("nimi").value;
-      var matric = document.getElementById("matrikkel").value;
-      if (name && matric) {
-        if (matric.match(new RegExp("^[A-ZÕÜÖÄ][0-9]+$"))) {
-          this.sendRequest(name, matric);
-        }
-        else
-          alert("Matriklinumber peab koosnema ainult SUUREST esitähest ja numbritest.");
-      }
-      else
-        alert(`Väljad nimi ja matriklinumber peavad olema täidetud!`);
-    },
-  },
-  components: { Banner }
+  components: { Banner, LoginButtons }
 };
 </script>
 
 <template>
-  <div class="header">
+  <div class="body">
     <div>
       <Banner />
       <h2>
@@ -99,26 +37,7 @@ export default {
         Muutes matriklit luuakse uus ühendus uude arvutisse. 24h pärast viimast
         sisselogimist kustutatakse masin koos failidega.
       </p>
-      <div style="width: 70%; margin: auto; border: lightgrey 1px solid">
-        <div style="display: flex; margin: auto">
-          <span class="loginInput">
-            <label for="nimi">Nimi:</label>
-            <input type="text" id="nimi" />
-          </span>
-          <span class="loginInput">
-            <label for="matrikkel">Matrikkel:</label>
-            <input type="text" id="matrikkel" />
-          </span>
-        </div>
-        <div style="display: flex; margin: auto">
-          <button id="authButton" @click="validateLoginCredentials" style="margin-left: auto; margin-right: 1em">
-            Logi sisse
-          </button>
-          <button id="anonButton" @click="sendRequest(null, null)" style="margin-left: 1em; margin-right: auto">
-            Jätka külalisena
-          </button>
-        </div>
-      </div>
+      <LoginButtons />
     </div>
   </div>
 </template>
@@ -128,6 +47,12 @@ h2 {
   margin: 4vh auto;
   text-align: center
 }
-.header{padding: 2vh 2vw}
-h3{text-align: center}
+
+.body {
+  padding: 2vh 2vw
+}
+
+h3 {
+  text-align: center
+}
 </style>
