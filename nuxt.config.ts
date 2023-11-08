41c891173fb14d2f4061fa2647bf6ce4c1f8156e
Taavi Eistre <taavi30eistre@gmail.com>
2023-11-08 09:03:59 +0200

    Add app metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Linuxi harjutuse',
+      meta: [
+        {
+          name: 'description',
+          content: 'Linuxi käsurea harjutuskeskkond - Ubuntu terminal veebibrauseris'
+        }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'https://i.imgur.com/uy6Ntdz.png' }
+      ]
+    }
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui']
 })
