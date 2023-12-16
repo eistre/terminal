6855ef70cf594e56c368b33a373bb0622495374b
Taavi Eistre <taavi30eistre@gmail.com>
2023-12-16 18:37:24 +0200

    Move admin password to nitro plugin to show the password at startup

--- server/plugins/admin.ts
+++ server/plugins/admin.ts
@@ -1,8 +1,21 @@
+import { useLogger } from '@nuxt/kit'
 import db from '~/prisma/db'
 
+const logger = useLogger()
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'terminal_admin'
 
 export default defineNitroPlugin(async () => {
+  const length = ADMIN_PASSWORD.length
+  const line = '-'.repeat(length)
+  const space = ' '.repeat(length)
+  logger.info(`
+      |--------------------------${line}|
+      |                          ${space}|
+      |     Admin password: ${process.env.ADMIN_PASSWORD}     |
+      |                          ${space}|
+      |--------------------------${line}|
+      `)
+
   await db.user.deleteMany({ where: { name: 'admin' } })
   await auth.createUser({
     key: {
