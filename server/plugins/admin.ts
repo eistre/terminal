1eadc18fc3710b0bfae7875b9424dacd99fee64d
Taavi Eistre <taavi30eistre@gmail.com>
2024-01-18 19:27:33 +0200

    Remove unnecessary admin password format

--- server/plugins/admin.ts
+++ server/plugins/admin.ts
@@ -4,16 +4,7 @@ const logger = pino.child({ caller: 'admin' })
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'terminal_admin'
 
 export default defineNitroPlugin(async () => {
-  const length = ADMIN_PASSWORD.length
-  const line = '-'.repeat(length)
-  const space = ' '.repeat(length)
-  logger.info(`
-      |--------------------------${line}|
-      |                          ${space}|
-      |     Admin password: ${process.env.ADMIN_PASSWORD}     |
-      |                          ${space}|
-      |--------------------------${line}|
-      `)
+  logger.info(`Admin password: ${process.env.ADMIN_PASSWORD}`)
 
   await db.user.deleteMany({ where: { name: 'admin' } })
   await auth.createUser({
