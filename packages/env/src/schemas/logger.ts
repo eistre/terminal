61318cd724dd0d6d491b26176ddc482237dbcdc2
Taavi Eistre <taavi30eistre@gmail.com>
2025-12-18 23:36:10 +0200

    feat: extend logger schema to include 'trace' log level

--- packages/env/src/schemas/logger.ts
+++ packages/env/src/schemas/logger.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const loggerSchema = z.object({
-  LOG_LEVEL: z.enum(['error', 'warn', 'info', 'debug']).default('info'),
+  LOG_LEVEL: z.enum(['error', 'warn', 'info', 'debug', 'trace']).default('info'),
   NODE_ENV: z.enum(['development', 'test', 'production']),
 });
 
